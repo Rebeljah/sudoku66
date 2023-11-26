02f69b7cb1d762acce8bce657d4bd92e1fb7dea3
rebeljah <rebeljah@gmail.com>
2023-11-25 22:36:55 -0500

    added comments, removed dead code

--- src/main.py
+++ src/main.py
@@ -1,34 +1,28 @@
 import pygame as pg
 
 class GameState:
+    """Class to manage and update game objects and draw to screen."""
     def __init__(self, screen):
         self.screen = screen
 
     def check_events(self):
-        pass
+        """Check pygame events and update game state / objects"""
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
 
     def update(self):
+        """This method runs on each frame to make updates to game objects like
+        the Board"""
         pass
 
     def draw(self):
+        """Draw the UI onto the pygame screen"""
         self.screen.flip()
 
 
-def check_events(game: GameState):
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-
-
-def update_game(game: GameState):
-    pass
-
-
-def draw_screen(game: GameState, screen: pg.Surface):
-    pass
-
-
 def new_screen() -> pg.Surface:
+    """Creates a new pygame screen based on the user's monitor size"""
     monitor_height = pg.display.Info().current_h
     height = width = 0.45 * monitor_height
 
